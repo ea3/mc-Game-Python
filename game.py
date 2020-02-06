dc234c2f4cf4cf04e75a07c03d10676c64b04403
Emilio Araos <ea3dev@gmail.com>
2020-02-05 22:22:22 -0700

    getters and setters example

---
+++ game.py
@@ -0,0 +1,24 @@
+class Player(object):
+
+    def __init__(self, name):
+        self.name = name        # Data attributes
+        self._lives = 3          # data attributes
+        self.level = 1          # data attributes
+        self.score = 0          # data attributes
+
+    def _get_lives(self):
+        return self._lives
+
+    def _set_lives(self, lives):
+        if lives >= 0:
+            self._lives = lives
+        else:
+            print("Lives can't be negative")
+            self._lives = 0
+
+    lives = property(_get_lives, _set_lives)
+
+    def __str__(self):
+        return "Name: {0.name}, Lives: {0.lives}, Level: {0.level}, Score {0.score}".format(self)
+
+
---
+++ main.py
@@ -0,0 +1,18 @@
+from game import Player
+
+Emilio = Player("Emilio")
+
+print(Emilio.name)
+print(Emilio.lives)
+print(Emilio)
+
+Emilio.lives -= 1
+print(Emilio)
+
+Emilio.lives -= 1
+print(Emilio)
+
+Emilio.lives -= 1
+print(Emilio)
+
+
