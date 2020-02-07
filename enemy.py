8dbb28673f57d2132d944f8e14d4e787fe796ffa
Emilio Araos <ea3dev@gmail.com>
2020-02-07 03:27:59 -0700

    creating enemy class

---
+++ enemy.py
@@ -0,0 +1,9 @@
+class Enemy:
+
+    def __init__(self, name="Enemy", hit_points=0, lives=1):
+        self.name = name
+        self.hit_points = hit_points
+        self.lives = lives
+
+        
+
