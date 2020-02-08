ce8b11dda2911768a1498286745da96e0cf27258
Emilio Araos <ea3dev@gmail.com>
2020-02-07 21:42:05 -0700

    inheritance in Python

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from enemy import Enemy, Troll
 
 
-ugly_troll = Troll()
+ugly_troll = Troll("Pug")
 print("Ugly Troll - {}". format(ugly_troll))
 
-another_troll = Troll("Ug", 18, 1)
-print("Another Troll - {}". format(another_troll), end='')
+another_troll = Troll("Ug")
+print("Another Troll - {}". format(another_troll))
 
-brother = Troll("Urg", 23)
+brother = Troll("Urg")
 print(brother)
+
+ugly_troll.grunt()
+
+another_troll.grunt()
+brother.grunt()
+
