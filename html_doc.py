594368465eb9fcfd1abee65983a3b95ebe9bce0f
Emilio Araos <ea3dev@gmail.com>
2020-02-07 23:45:23 -0700

    Created Tag and DOctype classes

---
+++ html_doc.py
@@ -0,0 +1,19 @@
+class Tag(object):
+
+    def __init__(self, name, contents):
+        self.start_tag = '<{}>'.format(name)
+        self.end_tag = '</{}>'.format(name)
+        self.contents = contents
+
+    def __str__(self):
+        return "{0.start_tag}{0.contents}{0.end_tag}".format(self)
+
+    def display(self):
+        print(self)
+
+
+class DocType(Tag):
+
+    def __init__(self):
+        super().__init__('!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" http://www.w3.org/TR/html4/strict.dtd', '')
+        self.end_tag = ''       # DOCTYPE does not have and end tag
