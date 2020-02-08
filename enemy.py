b43d252d9dadad8f7251d34a18620cac1ff15214
Emilio Araos <ea3dev@gmail.com>
2020-02-07 22:07:43 -0700

    created Vampire class and dodges method

--- enemy.py
+++ enemy.py
@@ -1,9 +1,13 @@
+import random
+
+
 class Enemy:
 
     def __init__(self, name="Enemy", hit_points=0, lives=1):
         self.name = name
         self.hit_points = hit_points
         self.lives = lives
+        self.alive = True
 
     def take_damage(self, damage):
         remaining_points = self.hit_points - damage
@@ -12,6 +16,11 @@ class Enemy:
             print("I took {} points damage and have {} left".format(damage, self.hit_points))
         else:
             self.lives -= 1
+            if self.lives > 0:
+                print("{0.name} lost a life".format(self))
+            else:
+                print("{0.name} is dead".format(self))
+                self.alive = False
 
     def __str__(self):
         return "Name: {0.name}, Lives: {0.lives}, Hit_Points {0.hit_points}".format(self)
@@ -24,4 +33,49 @@ class Troll(Enemy):
 
     def grunt(self):
         print("Me {0.name}. {0.name} stomp you".format(self))
+
+
+class Vampire(Enemy):
+
+    def __init__(self, name):
+        super().__init__(name=name, hit_points=12, lives=3)
+
+    def dodges(self):
+
+        if random.randint(1,3) == 3:
+            print("****** {0.name} dodges *******".format(self))
+            return True
+        else:
+            return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from enemy import Enemy, Troll
+from enemy import Enemy, Troll, Vampire
 
 
 ugly_troll = Troll("Pug")
 print("Ugly Troll - {}". format(ugly_troll))
+ugly_troll.take_damage(3)
 
 another_troll = Troll("Ug")
 print("Another Troll - {}". format(another_troll))
@@ -15,3 +16,15 @@ ugly_troll.grunt()
 another_troll.grunt()
 brother.grunt()
 
+big_Vampire = Vampire("Draculator")
+print(big_Vampire)
+big_Vampire.take_damage(7)
+
+small_Vampire = Vampire("Draculin")
+print(small_Vampire)
+small_Vampire.take_damage(2)
+
+print("*" * 80)
+while big_Vampire.alive:
+    big_Vampire.take_damage(1)
+    print(big_Vampire)
